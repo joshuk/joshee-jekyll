36d8023d9694567857396854b986bc6835dcadcf
joshuk <25204957+joshuk@users.noreply.github.com>
2020-07-10 21:23:19 +0100

    Add the _site folder to the repo

---
+++ _site/assets/js/post.js
@@ -0,0 +1,5 @@
+j('body')[0].classList.remove('no-js');
+
+new LazyLoad({
+    elements_selector: ".lazy"
+});
---
+++ _site/assets/js/utils.js
@@ -0,0 +1,1 @@
+const j = (selector) => { return document.querySelectorAll(selector); }
