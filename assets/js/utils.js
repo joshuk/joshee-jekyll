af8150947c3b3ab3e02f5275e801e7c79a1d0776
joshuk <25204957+joshuk@users.noreply.github.com>
2020-07-15 17:34:26 +0100

    Fix flashing on Firefox (and add _site to .gitignore)

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -3,6 +3,11 @@ document.querySelector('.colour-scheme-switch').addEventListener('click', () => 
     let value = (1 - document.querySelectorAll('body.dark').length);
     document.cookie = 'darkMode=' + value + ';path=/;';
 
+    document.querySelector('body').classList.add('transition');
+    setTimeout(() => {
+        document.querySelector('body').classList.remove('transition');
+    }, 500);
+
     document.querySelector('body').classList.remove(classes[1 - value]);
     document.querySelector('body').classList.add(classes[value]);
 });
