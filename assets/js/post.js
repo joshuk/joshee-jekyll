87a9a66ee646b436582275ca6ed7626d5a806422
joshuk <25204957+joshuk@users.noreply.github.com>
2020-07-15 13:57:12 +0100

    Add darkmode toggle

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,5 +1,3 @@
-document.querySelector('body').classList.remove('no-js');
-
 new LazyLoad({
     elements_selector: ".lazy"
 });
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,8 @@
-const j = (selector) => { return document.querySelectorAll(selector); }
+document.querySelector('.colour-scheme-switch').addEventListener('click', () => {
+    let classes = ['light', 'dark'];
+    let value = (1 - document.querySelectorAll('body.dark').length);
+    document.cookie = 'darkMode=' + value + ';path=/;';
+
+    document.querySelector('body').classList.remove(classes[1 - value]);
+    document.querySelector('body').classList.add(classes[value]);
+});
